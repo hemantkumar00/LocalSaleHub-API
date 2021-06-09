af0778eeee25f6811cab1a73c985f106845c15ff
hemantkumar00 <hemant.19jics091@jietjodhpur.ac.in>
2021-06-09 06:28:41 +0530

    master

---
+++ routes/messages.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const express = require('express');
+const Message = require('../models/Message');
+const router = express.Router();
+
+//add
+
+router.post('/', (req, res, next) => {});
+
+//get
+
+module.exports = router;
